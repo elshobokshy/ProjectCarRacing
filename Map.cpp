05447672f4854f95d708e9c4a59263878c1b2d43
Creusel_Victor <victor.creusel4@etu.univ-lorraine.fr>
2015-10-01 15:44:36 +0200

    Added the Map class, it's structure.

---
+++ Map.cpp
@@ -0,0 +1,26 @@
+#include "Map.hpp"
+#include <string>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+
+Map::Map()
+{
+}
+
+
+Map::Map(const std::string &fileName)
+{
+	//loadFromFile(fileName);
+}
+
+
+void loadFromFile(const std::string &fileName)
+{
+	//TODO
+}
+
+
+void Map::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+	//TODO	
+}
---
+++ Map.hpp
@@ -0,0 +1,43 @@
+/*
+* The map will encapsulate the road blocks, and furthermore ways to load it and some interactions with the cars.
+*/
+#ifndef DEF_MAP_H
+#define DEF_MAP_H
+
+
+#include <SFML/Graphics/RenderStates.hpp>
+//#include "RoadBlock.hpp"
+
+
+
+namespace std
+{
+	class string;
+}
+
+namespace sf
+{
+	class RenderTarget;
+}
+
+
+
+class Map : public sf::Drawable
+{
+	public:
+		Map();
+		Map(const std::string &fileName);
+
+		void loadFromFile(const std::string &fileName);
+		
+
+
+
+
+		virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+	private:
+		
+};
+
+
+#endif
