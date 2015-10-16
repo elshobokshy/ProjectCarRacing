28841853400befdaa67d1bf64715c84727ebdb07
elshobokshy <islam20088@hotmail.com>
2015-10-16 10:30:42 +0200

    Adding windows/ubuntu conditional compilation because working on different things.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int main(int argc, char **argv)
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Car Racing");
 
+	#ifdef _WIN32
+	game(window);
+	#endif
+	
+	#ifdef __gnu_linux__
 	mapEdit::mapEdit(window);
 	//game(window);
 
