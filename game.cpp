9a4a719906ae64af80fc785667745f5b6f4785bd
Creusel_Victor <victor.creusel4@etu.univ-lorraine.fr>
2015-11-15 21:38:10 +0100

    Trying to find where the error comes from. Confirmed that there must be a problem while initializing the lineHitBoxes.

--- game.cpp
+++ game.cpp
@@ -75,12 +75,6 @@ namespace game
 
 	void game(sf::RenderWindow &window)
 	{
-		/*sf::Vector2f p(1, 0);
-		sf::Transform T;
-		T.rotate(180);
-		p = T * p;
-		std::cout<< p.x << " ; "<< p.y<< '\n';*/
-
 		//view that will follow the car
 		sf::View carView(sf::FloatRect(0, 0, 800, 600));
 		//carView.setSize(640, 480);
@@ -128,12 +122,11 @@ namespace game
 			for(Map::iterator it = map.begin(); it != map.end() && !collided; it++)
 			{	
 				collided = collision::collision(playerCar.getHitBox(), it->getHitBox());
-				//std::cout<< i<< '\n';
 				i++;
 			}
 			if(collided)
 			{
-				std::cout<< i<<"\n";
+				//std::cout<< i<<"\n";
 			}
 
 			// \game physics /////////////////////////
